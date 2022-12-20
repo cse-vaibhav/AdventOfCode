999dbb9ab5bacff132af5e94433efaf03af99899
cse-vaibhav <jainvaibhav671@gmail.com>
2022-12-20 21:30:48 +0530

    completed day20 part2

--- python/2022/day20/solution.py
+++ python/2022/day20/solution.py
@@ -3,9 +3,10 @@ from rich import print
 import sys
 
 def parseInput():
+	key = 811589153;
 	filename = sys.argv[1];
 	with open(filename, 'r') as f:
-		return [ int(line.strip()) for line in f.readlines() ]
+		return [ int(line.strip()) * key for line in f.readlines() ]
 
 def shift(inp: list, curr: tuple):
 	ind: int = inp.index(curr);
@@ -13,14 +14,15 @@ def shift(inp: list, curr: tuple):
 	new_ind: ind = (ind+val)%len(inp);
 	inp.insert(new_ind, curr);
 
-def mix(inp: list):
+def mix(inp: list, rounds: int = 1):
 	inp = list(enumerate(inp));
 	orig: list = inp.copy();
-	n: int = inp[-1][0] + 1;
-	for pair in orig:
+	n: int = orig[-1][0] + 1;
 
-		curr: int = inp.index(pair);
-		shift(inp, pair);
+	for r in range(rounds):
+		for pair in orig:
+			curr: int = inp.index(pair);
+			shift(inp, pair);
 
 	return inp;
 
@@ -40,4 +42,20 @@ def part1():
 	print(loc)
 	print(ans)
 
-part1();
+def part2():
+	inp: list = list(parseInput());
+	n: int = len(inp);
+
+	zero: tuple = (inp.index(0), 0);
+	inp = mix(inp, 10);
+
+	zero_ind: int = inp.index(zero);
+
+	x = [1000, 2000, 3000];
+	loc = [inp[(zero_ind+y)%n] for y in x ]
+	ans: int = sum([p[1] for p in loc])
+
+	print(loc)
+	print(ans)
+
+part2();
