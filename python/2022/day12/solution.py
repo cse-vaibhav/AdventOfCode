b7d7ca9be94a9cedff25522b40483a441aaa64af
cse-vaibhav <jainvaibhav671@gmail.com>
2023-01-02 16:30:42 +0530

    Completed day12 (aoc22)

--- python/2022/day12/solution.py
+++ python/2022/day12/solution.py
@@ -26,7 +26,7 @@ def log(*args) -> None:
     rich.print(*args)
 
 
-class Solution:
+class Part1:
     def __init__(self):
         self.inputFile = sys.argv[1]
         self.visited = set()
@@ -47,15 +47,6 @@ class Solution:
         self.visited.remove(self.start)
         self.nodes_roamed = self.min_steps
         self.solve("E")
-
-    @timeit
-    def part2(self):
-        # self.start = self.solve("e")
-        # print()
-        # self.visited.remove(self.start)
-        # self.nodes_roamed = self.min_steps
-        self.solve("E")
-        return
 
     def parseInput(self) -> list[list[str]]:
         with open(self.inputFile, "r", encoding="utf-8") as inp:
@@ -131,4 +122,119 @@ class Solution:
                     heappush(pq, (steps + 1, neighbour))
 
 
-solution = Solution().part1()
+class Part2:
+    def __init__(self):
+        self.inputFile = sys.argv[1]
+        self.visited = set()
+        self.grid = self.parseInput()
+
+        self.rows = len(self.grid)
+        self.cols = len(self.grid[0])
+
+        self.max = int(1e30)
+        self.min_steps: int = self.max
+        self.nodes_roamed: int = 0
+
+    @timeit
+    def part2(self):
+
+        aNodes = [
+            (x, y)
+            for x in range(self.rows)
+            for y in range(self.cols)
+            if self.grid[x][y] in ["S", "a"]
+        ]
+
+        for a in aNodes:
+
+            self.visited.clear()
+            self.start = a
+            steps, self.start = self.solve("e", 0)
+
+            if self.start == None:
+                continue
+
+            self.visited.remove(self.start)
+            self.nodes_roamed = steps
+            steps, _ = self.solve("E", steps)
+            self.min_steps = min(steps, self.min_steps)
+        print(f"Steps: ", self.min_steps)
+
+    def parseInput(self) -> list[list[str]]:
+        with open(self.inputFile, "r", encoding="utf-8") as inp:
+            lines: list = []
+            row: int = 0
+            for line in inp.readlines():
+                line = line.strip()
+                lines.append(line)
+                if line.count("S") != 0:
+                    self.start = (row, line.index("S"))
+
+                row += 1
+
+            log(lines)
+            log(self.start)
+            return lines
+
+    def getValidNeighbours(self, currPos: tuple) -> tuple:
+        neighbours: set = {(1, 0), (-1, 0), (0, -1), (0, 1)}
+
+        cx, cy = currPos
+        curr_height = self.grid[cx][cy]
+        if curr_height == "S":
+            curr_height = "a"
+        elif curr_height == "E":
+            curr_height = "z"
+
+        next_neighbours = []
+        for n in neighbours:
+            nx, ny = (cx + n[0], cy + n[1])
+
+            if (
+                not (0 <= nx < self.rows and 0 <= ny < self.cols)
+                or (nx, ny) in self.visited
+            ):
+                continue
+
+            neighbour_height = self.grid[nx][ny]
+            if neighbour_height == "S":
+                neighbour_height = "a"
+            elif neighbour_height == "E":
+                neighbour_height = "z"
+
+            if ord(neighbour_height) - ord(curr_height) > 1:
+                continue
+
+            next_neighbours.append((nx, ny))
+
+        return next_neighbours
+
+    def solve(self, final_char: str, steps: int):
+        self.visited.clear()
+
+        pq = [(steps, self.start)]
+        min_steps = self.max
+        while True:
+            if not pq:
+                break
+
+            steps, node = heappop(pq)
+            if node not in self.visited:
+                self.nodes_roamed += 1
+                self.visited.add(node)
+                x, y = node
+                if self.grid[x][y] == final_char:
+                    min_steps = min(min_steps, steps)
+                    log(f"Steps '{final_char}' : ", min_steps)
+                    log(f"Nodes Roamed: '{self.nodes_roamed}'")
+                    return [min_steps, (x, y)]
+
+                neighbours = self.getValidNeighbours(node)
+                for neighbour in neighbours:
+                    heappush(pq, (steps + 1, neighbour))
+        return [0, None]
+
+
+if __name__ == "__main__":
+    part1 = Part1().part1()
+    part2 = Part2().part2()
